2fd986f1c83bf9059e51fb734b78779f9c935143
Souleymane Abdou <souleymaneah98@gmail.com>
2023-07-20 10:57:29 +0100

    reajustement

---
+++ bka/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import *
+
+class installation_informationForm(forms.ModelForm):
+    class Meta:
+        model = installation_information
+        fields = ['lat', 'lon']
--- bka/models.py
+++ bka/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class installation_information(models.Model):
+    lat = models.FloatField(max_length=20, null=True)
+    lon = models.FloatField(max_length=20, null=True)
+    debit = models.FloatField(max_length=8, default=0.0, null=True)
+    CHOIX = (
+        ('Ko', 'Ko'),
+        ('Mo', 'Mo'),
+        ('Go', 'Go'),
+    )
+    measure = models.CharField(max_length=10, choices=CHOIX, default='Mo', null=True)
+
+    date_ajout = models.DateTimeField(auto_now_add=True)
+    date_modif = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.lat) + ' ' + str(self.lon)
+    
+

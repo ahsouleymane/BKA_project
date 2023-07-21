7133cf45a5d3efb1d5390bd02bfd33d380778554
Souleymane Abdou <souleymaneah98@gmail.com>
2023-07-21 17:30:15 +0100

    templates

--- bka/decorators.py
+++ bka/decorators.py
@@ -23,7 +23,22 @@ def allowed_users(allowed_roles=[]):
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
-                return redirect('etudiant_list')
+                return redirect('user_page')
                 
         return wrapper_func
     return decorator
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+    
+        group = None
+
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        
+        if group == 'customer':
+            return redirect('user_page')
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+            
+    return wrapper_func
--- bka/views.py
+++ bka/views.py
@@ -15,23 +15,46 @@ def add_installation_information(request):
     return render(request, '', context)
 
 def edit_installation_information(request, pk):
-    information = installation_information.objects.get(id=pk)
-    form = installation_informationForm(instance=information)
-    if request.method == 'POST':
-        form = installation_informationForm(request.POST, instance=information)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
+    try:
+        information = installation_information.objects.get(id=pk)
+    except installation_information.DoesNotExist:
+        information = None
+
+    if information.status == False:
+        information.status = True
+        form = installation_informationForm(instance=information)
+        if request.method == 'POST':
+            form = installation_informationForm(request.POST, instance=information)
+            if form.is_valid():
+                form.save()
+            return redirect('/')
+        
     context = {'form': form}
     return render(request, '', context)
 
-def list_coordinates(request):
-    list = installation_information.objects.all()
+def list_coordinates(request, pk):
+    try:
+        information = installation_information.objects.get(id=pk)
+    except installation_information.DoesNotExist:
+        information = None
+
+    list = []
+    if information.status == False:
+        list = information
+
     context = {'list': list}
     return render(request, '', context)
 
-def list_all_information(request):
-    list = installation_information.objects.all()
+def list_all_informations(request, pk):
+    try:
+        information = installation_information.objects.get(id=pk)
+    except installation_information.DoesNotExist:
+        information = None
+
+    list = []
+    if information.status == True:
+        list = information
+
     context = {'list': list}
     return render(request, '', context)
 
